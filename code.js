6f110107a44a5915601aacf10c393cb548f282ca
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-06-03 15:47:50 +0000

    deploy: 75b1c2fc6ea86bd53023527fd261b01e4964ba60

--- code.js
+++ code.js
@@ -1,4 +1,31 @@
 // To run this assignment, right click on index.html in the Visual Studio file explorer to the left
 // and select "Open with Live Server"
+let highestNumber = prompt("What's the highest number?")
+// Your Code Here
+function snapCrackle(maxValue){
+    let answer = ""
 
-// Your Code Here.
+    for(let i = 1; i <= maxValue; i += 1){
+        if(i % 2 == 1 && i % 5 == 0){
+            answer += "SnapCrackle, "
+        }
+        else if(i % 2 == 1){
+            answer += "Snap, "
+        }
+        else if(i % 5 == 0){
+            answer += "Crackle, "
+        }
+        else {
+            answer += `${i}, `
+        }
+    }
+    return answer
+}
+
+function render(text, maxValue){
+    document.write(`<h1>SnapCrackle!</h1><h3>Max Value: ${maxValue}</h3><p>${text}</p>`)
+}
+
+let results = snapCrackle(highestNumber)
+
+render(results, highestNumber)
